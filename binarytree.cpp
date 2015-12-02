94e9dca1a3ea3161108dc36ba2211a9546357480
standon <anthonygbostock@gmail.com>
2015-12-02 09:33:11 -0800

    notes on BST / lab 25

---
+++ binarytree.cpp
@@ -0,0 +1,26 @@
+to delete binary tree
+use search function
+if leaf has no child
+if (l == NULL && r == NULL) //nochildren
+elseif(l == NULL)
+//has right child
+
+elseif(r == NULL)
+//has left child
+
+else
+//2 children
+delete root 
+root = null
+
+if node has one child
+point root of subtree to child of subtree
+algorithm
+temp pointer to the one you will delete
+point grandfather to grandchild
+delete temp pointer
+
+How can you hold string values in BST?
+if node has two children
+
+
